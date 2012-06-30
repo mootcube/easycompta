ccbddede9f566d0018e9612a0c292d848280a8c1
MoOT <mathieu.chataigner@gmail.com>
2012-07-01 00:11:51 +0200

    junit 4 tests

--- src/test/java/com/moot/easycompta/EasyComptaTest.java
+++ src/test/java/com/moot/easycompta/EasyComptaTest.java
@@ -1,36 +1,21 @@
 package com.moot.easycompta;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+//import junit.framework.Test;
+//import junit.framework.TestCase;
+//import junit.framework.TestSuite;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
  */
 public class EasyComptaTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public EasyComptaTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( EasyComptaTest.class );
-    }
-
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp()
     {
         assertTrue( true );
---
+++ src/test/java/com/moot/easycompta/MainTest.java
@@ -0,0 +1,14 @@
+package com.moot.easycompta;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void testMain() {
+		//fail("Not yet implemented");
+	}
+
+}
